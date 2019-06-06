8a4521b8e560ac297128ba1df7fc3fede991f74a
Arunkumar Sadasivan <contact.arunsec@gmail.com>
2019-06-06 11:44:09 -0500

    adding datatables and js files for easyreport

---
+++ assets/js/custom_report.js
@@ -0,0 +1,41 @@
+/* contact.arunsec@gmail.com */
+
+$(document).ready(function() {
+	$('#example').DataTable();
+});
+
+
+
+/*
+$(document).ready(function() {
+    var table = $('#example').DataTable();
+     
+    $('#example tbody').on('click', 'tr', function () {
+        var data = table.row( this ).data();
+        alert( 'You clicked on '+ data[2] +'\'s row' );
+    } );
+} );
+
+*/
+
+
+/*
+$("body").on("click", "td", function() {
+    alert($( this ).text());
+});
+
+
+/*
+function toggleDiv(divId) {
+	$("#"+divId).toggle();
+}
+*/
+
+/*
+$( "button" ).click(function() {
+	  $( "div" ).toggle();
+});
+*/
+
+
+ 
